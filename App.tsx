776c394832c2f93547c406f1fd51ebf2ffcb6d2b
fmoraless <franticzero@gmail.com>
2023-12-31 19:16:49 -0300

    vector icons implementation

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import {View, Text} from 'react-native';
 import React from 'react';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 const App = () => {
   return (
     <View>
       <Text>App</Text>
+      <Icon name="airplane-outline" size={80} color="black" />
     </View>
   );
 };
