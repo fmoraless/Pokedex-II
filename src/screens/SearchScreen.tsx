b31327af25932e31d016a15244268cc6e40096d6
fmoraless <franticzero@gmail.com>
2024-01-22 22:26:06 -0300

    Add usePokemonSearch hook and display Pokemon list in SearchScreen

--- src/screens/SearchScreen.tsx
+++ src/screens/SearchScreen.tsx
@@ -1,10 +1,30 @@
-import {View, Text, Platform} from 'react-native';
+import {
+  View,
+  Text,
+  Platform,
+  ActivityIndicator,
+  StyleSheet,
+  FlatList,
+} from 'react-native';
 import React from 'react';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
 import {SearchInput} from '../components/SearchInput';
+import {usePokemonSearch} from '../hooks/usePokemonSearch';
+import {styles as globalStyles} from '../../theme/appTheme';
+import PokemonCard from '../components/PokemonCard';
 
 const SearchScreen = () => {
   const {top} = useSafeAreaInsets();
+  const {isFetching, simplePokemonList} = usePokemonSearch();
+
+  if (isFetching) {
+    return (
+      <View style={styles.activityContainer}>
+        <ActivityIndicator color="grey" size={50} />
+        <Text style={{marginTop: 3}}>Cargando...</Text>
+      </View>
+    );
+  }
 
   return (
     <View
@@ -14,8 +34,35 @@ const SearchScreen = () => {
         marginHorizontal: 20,
       }}>
       <SearchInput />
+
+      <FlatList
+        data={simplePokemonList}
+        keyExtractor={pokemon => pokemon.id}
+        showsVerticalScrollIndicator={false}
+        numColumns={2}
+        ListHeaderComponent={
+          <Text
+            style={{
+              ...globalStyles.title,
+              ...globalStyles.globalMargin,
+              top: top + 20,
+              marginBottom: top + 20,
+              paddingBottom: 10,
+            }}>
+            Pokedex
+          </Text>
+        }
+        renderItem={({item}) => <PokemonCard pokemon={item} />}
+      />
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  activityContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 export default SearchScreen;
